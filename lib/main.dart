c49a7247a00969c593c836225cc8b407058ee366
legendlo <louis.legendre@reseau.eseo.fr>
2023-12-11 15:38:21 +0100

    add basic pages structure & logo in assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:master_quiz/ui/screens/home.dart';
+import 'package:master_quiz/ui/screens/choose_options.dart';
+import 'package:master_quiz/ui/screens/question_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      title: 'Master Quizz',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/home': (context) => const Home(),
+        '/question': (context) => const QuestionPage(),
+        '/result': (context) => const ChooseOptions(),
+      },
+      initialRoute: '/home',
     );
   }
 }
---
+++ lib/ui/screens/choose_options.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseOptions extends StatefulWidget {
+  const ChooseOptions({super.key});
+
+  @override
+  State<ChooseOptions> createState() => _ChooseOptionsState();
+}
+
+class _ChooseOptionsState extends State<ChooseOptions> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
