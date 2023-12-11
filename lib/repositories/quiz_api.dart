33a65ead96704384fb527652929a4e7ecbcef4c0
Zilba26 <basile.lebaudy@gmail.com>
2023-12-11 15:45:25 +0100

    api done

--- lib/repositories/quiz_api.dart
+++ lib/repositories/quiz_api.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:master_quiz/models/difficulty.dart';
 import 'package:master_quiz/models/quiz.dart';
+import 'package:master_quiz/models/category.dart';
 
 class QuizApi {
   static const String baseUrl = 'https://quizzapi.jomoreschi.fr/api/v1/quiz';
@@ -19,5 +21,17 @@ class QuizApi {
     } else {
       throw Exception('Failed to load quiz');
     }
+  }
+
+  Future<List<Quiz>> getQuizByCategory(Category category, [int? limit]) async {
+    return await getQuiz('?category=${category.key}${limit == null ? '' : '&limit=$limit'}');
+  }
+
+  Future<List<Quiz>> getQuizByDifficulty(Difficulty difficulty, [int? limit]) async {
+    return await getQuiz('?difficulty=${difficulty.key}${limit == null ? '' : '&limit=$limit'}');
+  }
+
+  Future<List<Quiz>> getQuizByCategoryAndDifficulty(Category category, Difficulty difficulty, [int? limit]) async {
+    return await getQuiz('?category=${category.key}&difficulty=${difficulty.key}${limit == null ? '' : '&limit=$limit'}');
   }
 }
