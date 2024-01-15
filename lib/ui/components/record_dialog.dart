5a6e258e044d8fa641df8b7e9855f097eb0c4fbf
Zilba26 <basile.lebaudy@gmail.com>
2024-01-15 16:54:20 +0100

    record dialog finish

--- lib/ui/components/record_dialog.dart
+++ lib/ui/components/record_dialog.dart
@@ -3,73 +3,100 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:master_quiz/constantes.dart';
 import 'package:master_quiz/models/category.dart';
 import 'package:master_quiz/models/difficulty.dart';
+import 'package:master_quiz/ui/components/star_background.dart';
 
 import '../../blocs/record_cubit.dart';
 
 class RecordDialog extends StatelessWidget {
 
   final int score;
+  final int maxScore;
   final Category category;
   final Difficulty difficulty;
+  final bool? isRecord; //null value for equality
 
-  const RecordDialog({super.key, required this.score, required this.category, required this.difficulty});
+  const RecordDialog({super.key, required this.score, required this.category, required this.difficulty, required this.isRecord, required this.maxScore});
+
+  String breakRecordText() {
+    if (isRecord == null) {
+      return "Vous avez égalé\nvotre record";
+    } else if (isRecord!) {
+      return "Vous avez battu\nvotre record 😀";
+    } else {
+      return "Vous n'avez pas battu\nvotre record 😔";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
+    return Dialog(
       backgroundColor: backgroundColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
-      title: const Text(
-        'Quiz terminé',
-        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white), // Texte en blanc
-        textAlign: TextAlign.center,
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            "Score: $score",
-            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          const SizedBox(height: 20),
-          const Text("Vous avez battu votre record", style: TextStyle(fontSize: 16, color: Colors.white)),
-          const SizedBox(height: 10),
-          Column(
-            children: [
-              const Text(
-                'Record Actuel',
-                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.white),
+      child: BlocBuilder<RecordCubit, Map<int, Map<int, int?>>>(
+        builder: (context, state) {
+          return StarBackground(
+            animated: false,
+            child: SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Quiz terminé',
+                      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white), // Texte en blanc
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(text: "Score: $score", style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white)),
+                          TextSpan(text: " / $maxScore", style: const TextStyle(fontSize: 18, color: Colors.white))
+                        ]
+                      )
+                    ),
+                    const SizedBox(height: 20),
+                    Text(breakRecordText(),
+                        style: const TextStyle(fontSize: 17, color: Colors.white), textAlign: TextAlign.center,),
+                    const SizedBox(height: 20),
+                    BlocBuilder<RecordCubit, Map<int, Map<int, int?>>>(
+                      builder: (context, state) {
+                        return Text('${(isRecord != null && isRecord!) ? "Nouveau Record" : "Record Actuel"} : ${state[category.index]![difficulty.index]?.toString()}',
+                          style: const TextStyle(fontSize: 22.0, color: Colors.white),
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context, false);
+                            Navigator.pop(context);
+                          },
+                          child: const Text('ACCUEIL', style: TextStyle(color: Colors.white, fontSize: 15)),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context, true);
+                          },
+                          child: const Text('REJOUER', style: TextStyle(color: Colors.white, fontSize: 15)),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-              const SizedBox(height: 10.0),
-              BlocBuilder<RecordCubit, Map<int, Map<int, int?>>>(
-                builder: (context, state) {
-                  return Text(
-                    state[category.index]![difficulty.index] == null ? "Aucun" : state[category.index]![difficulty.index].toString(),
-                    style: const TextStyle(fontSize: 22.0, color: Colors.white),
-                  );
-                },
-              )
-            ],
-          ),
-        ],
+            ),
+          );
+        }
       ),
-      actionsAlignment: MainAxisAlignment.center,
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context, false);
-          },
-          child: const Text('Accueil', style: TextStyle(color: Colors.white)),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context, true);
-          },
-          child: const Text('Rejouer', style: TextStyle(color: Colors.white)),
-        ),
-      ],
     );
   }
 }
